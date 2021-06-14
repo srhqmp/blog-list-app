82806c98ef7fb439b93870b58344159a443aff77
srhqmp <ballanosarah@gmail.com>
2021-06-14 15:25:12 +0800

    modify gitignore

--- utils/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB_URI:
-    'mongodb://sarah:passwordko@clusters-shard-00-00.zetrn.mongodb.net:27017,clusters-shard-00-01.zetrn.mongodb.net:27017,clusters-shard-00-02.zetrn.mongodb.net:27017/blog-list-app?ssl=true&replicaSet=atlas-4i5z05-shard-0&authSource=admin&retryWrites=true&w=majority',
-  PORT: 3003,
-}
