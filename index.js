115f2343430f5cd2ee33fa7fecd65d74bb6d127f
srhqmp <ballanosarah@gmail.com>
2021-06-14 14:56:03 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb://sarah:passwordko@clusters-shard-00-00.zetrn.mongodb.net:27017,clusters-shard-00-01.zetrn.mongodb.net:27017,clusters-shard-00-02.zetrn.mongodb.net:27017/blog-list-app?ssl=true&replicaSet=atlas-4i5z05-shard-0&authSource=admin&retryWrites=true&w=majority'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
